a025fa4eb63f46e1e6aecb5bb85b7525b83b5ced
Jacob Erling <jacob@terrier.llc>
2025-03-06 18:38:48 -0500

    Set up API Key.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'c010a104',
+      s: 'avengers'
+    }
+  });
+
+  console.log(response.data);
+};
+
+fetchData();
