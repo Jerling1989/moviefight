9f89ccc73c0c7e4c2bea6f9895b7d8f0c3e532c0
Jacob Erling <jacob@terrier.llc>
2025-03-10 13:30:13 -0400

    remove "movie" references

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-const fetchData = async (searchTerm) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: 'c010a104',
-      s: searchTerm
-    }
-  });
-
-  if (response.data.Error) {
-    return [];
-  }
-
-  console.log(response.data.Search);
-  return response.data.Search;
-  
-};
 
 createAutoComplete({
   root: document.querySelector('.autocomplete'),
@@ -23,6 +7,26 @@ createAutoComplete({
       <img src="${imgSrc}" />
       ${movie.Title}
     `;
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'c010a104',
+        s: searchTerm
+      }
+    });
+
+    if (response.data.Error) {
+      return [];
+    }
+
+    return response.data.Search;
   }
 });
 
